ff1194cb8362aaf2af5c4f2fc0ffd5df38bae5e0
Shoaibali12 <sind98449@gmail.com>
2025-03-12 13:35:35 -0700

    setup backend and frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className=" bg-blue-900 text-white">
+        Hello welcome to skill swap platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
